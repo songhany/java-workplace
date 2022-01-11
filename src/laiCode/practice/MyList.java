03860fab38b41de6ccbee0dd731d384c0576c857
songhany <ysh961008@yeah.net>
2022-01-11 16:51:56 -0500

    List Interface

--- src/laiCode/practice/MyList.java
+++ src/laiCode/practice/MyList.java
@@ -1,5 +1,9 @@
 package laiCode.practice;
 
+import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public interface MyList {
     public boolean set(int index, int value);
     public Integer get(int index);
@@ -46,8 +50,12 @@ class LinkedList1 implements MyList {
 
 class Solution {
     public static void main(String[] args) {
-        // MyList l1 = new MyList();  // Wrong. MyList is Interface that cannot be instantiated
+        // MyList l1 = new MyList();  // Wrong. 不能new一个Interface和 abstract class. MyList is Interface that cannot be instantiated.
         MyList l2 = new LinkedList1();
         MyList l3 = new ArrayList1();
+
+        List<Integer> alist = new ArrayList<>();
+        List<Integer> alist2 = new LinkedList<>();
+        alist2.
     }
 }
